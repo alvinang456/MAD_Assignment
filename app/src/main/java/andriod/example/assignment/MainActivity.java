7814cea34e65e67cd54e36f49892c54620c729a4
alvin ang <alvinahc-wm19@student.tarc.edu.my>
2020-08-21 20:55:27 +0800

    Committed by Alvin

--- app/src/main/java/andriod/example/assignment/MainActivity.java
+++ app/src/main/java/andriod/example/assignment/MainActivity.java
@@ -1,30 +1,67 @@
 package andriod.example.assignment;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
     private Button button_login, button_signup;
+    private EditText email, password;
 
-
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        button_login = (Button) findViewById(R.id.button_login);
-        button_signup = (Button) findViewById(R.id.button_signup);
+        mAuth= FirebaseAuth.getInstance();
 
-        button_signup.setOnClickListener(new View.OnClickListener() {
+        email = (EditText)findViewById(R.id.username_input);
+        password = (EditText)findViewById(R.id.login_password_input);
+
+        button_login = (Button) findViewById(R.id.button_login);
+        button_login.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent (getApplicationContext(),RegisterAcvtivity.class);
+                mAuth.signInWithEmailAndPassword(email.getText().toString(), password.getText().toString())
+                        .addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(getApplicationContext(), "Log In success !", Toast.LENGTH_LONG).show();
 
-                startActivity(intent);
+                                } else {
+                                    Toast.makeText(getApplicationContext(), "Log In Failed !", Toast.LENGTH_LONG).show();
+                                }
+                            }
+                        });
+            }
+
+
+        });
+
+
+        button_signup = (Button) findViewById(R.id.button_signup);
+
+                button_signup.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+
+                        Intent intent = new Intent (getApplicationContext(),RegisterAcvtivity.class);
+
+                        startActivity(intent);
             }
         });
 
--- app/src/main/java/andriod/example/assignment/RegisterAcvtivity.java
+++ app/src/main/java/andriod/example/assignment/RegisterAcvtivity.java
@@ -1,11 +1,25 @@
 package andriod.example.assignment;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Date;
 
 public class RegisterAcvtivity extends AppCompatActivity {
     private Button CreateAccountButton;
@@ -14,14 +28,17 @@ public class RegisterAcvtivity extends AppCompatActivity {
     private EditText inputEmail;
     private EditText inputDateOfBirth;
     private RadioButton inputGenderMale, inputGenderFemale;
+    private FirebaseAuth mAuth;
+    private EditText inputUsername;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_acvtivity);
+        mAuth= FirebaseAuth.getInstance();
 
         CreateAccountButton = (Button) findViewById(R.id.button_register);
-        EditText inputUsername = (EditText) findViewById(R.id.register_username);
+         inputUsername = (EditText) findViewById(R.id.register_username);
         inputPassword = (EditText) findViewById(R.id.register_password);
         inputPhoneNumber = (EditText) findViewById(R.id.register_phone_number);
         inputEmail = (EditText) findViewById(R.id.register_email);
@@ -29,5 +46,38 @@ public class RegisterAcvtivity extends AppCompatActivity {
         RadioButton inputGenderFemale = (RadioButton) findViewById(R.id.rdbtn_gender_female);
         RadioButton inputGenderMale = (RadioButton) findViewById(R.id.rdbtn_gender_male);
 
+
+
+        CreateAccountButton.setOnClickListener(new View.OnClickListener() {
+            final String Password = inputPassword.getText().toString();
+            final String Username = inputUsername.getText().toString();
+            final String PhoneNumber = inputPhoneNumber.getText().toString();
+            final String Email = inputEmail.getText().toString();
+            final String DateOfBirth = inputDateOfBirth.getText().toString();
+            @Override
+            public void onClick(View view) {
+ /*               if (Username.equals("") || DateOfBirth.equals("") || PhoneNumber.equals("") || Email.equals("") || Password.equals("")) {
+                    Toast.makeText(getApplicationContext(), "Please Fill In The Blank "+Password+Username+Email+PhoneNumber+DateOfBirth, Toast.LENGTH_SHORT).show();
+
+
+                }else {*/
+                    mAuth.createUserWithEmailAndPassword(inputEmail.getText().toString(), inputPassword.getText().toString())
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if(task.isSuccessful()){
+                                    Toast.makeText(getApplicationContext(), "Sign Up Successfully !", Toast.LENGTH_SHORT).show();
+                                    Intent moveToSignIn = new Intent(getApplicationContext(),MainActivity.class);
+                                    startActivity(moveToSignIn);
+                                }else{
+                                    Toast.makeText(getApplicationContext(),"Error !",Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+                //}
+            };
+
+
+        });
     }
 }
